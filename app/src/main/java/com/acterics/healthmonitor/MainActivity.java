b27a3ab0dae2ee80f7b65b8919590e70298db5bd
oleg <lolego1601@gmail.com>
2017-05-12 13:09:16 +0300

    Initial commit

---
+++ app/src/main/java/com/acterics/healthmonitor/MainActivity.java
@@ -0,0 +1,12 @@
+package com.acterics.healthmonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
