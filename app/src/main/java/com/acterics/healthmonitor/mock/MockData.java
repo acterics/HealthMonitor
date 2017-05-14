2f3e3d5790a109d6c6c1198aef07f2883c62cefb
oleg <lolego1601@gmail.com>
2017-05-14 19:48:44 +0300

    mock issues, add issue view

---
+++ app/src/main/java/com/acterics/healthmonitor/mock/MockData.java
@@ -0,0 +1,32 @@
+package com.acterics.healthmonitor.mock;
+
+import com.acterics.healthmonitor.data.models.IssueModel;
+import com.acterics.healthmonitor.ui.issues.IssueCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by oleg on 14.05.17.
+ */
+
+public class MockData {
+    private static final String MOCK_TEXT = "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.";
+
+    private MockData() {
+
+    }
+
+    public static List<IssueModel> getIssues() {
+        List<IssueModel> issueModels = new ArrayList<>();
+        IssueModel issue;
+        for (int i = 0; i < 10; i++) {
+            issue = new IssueModel();
+            issue.setCategory(IssueCategory.STOMACH);
+            issue.setIssueTitle("Issue " + i);
+            issue.setIssueSummary(MOCK_TEXT);
+            issueModels.add(issue);
+        }
+        return issueModels;
+    }
+}
--- app/src/main/java/com/acterics/healthmonitor/ui/drawerfragments/IssuesFragment.java
+++ app/src/main/java/com/acterics/healthmonitor/ui/drawerfragments/IssuesFragment.java
@@ -1,8 +1,21 @@
 package com.acterics.healthmonitor.ui.drawerfragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by oleg on 13.05.17.
  */
 
-public class IssuesFragment {
+public class IssuesFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
